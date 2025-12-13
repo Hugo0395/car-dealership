fee9eb8133529e7f55e07bf27135dcfe88d1d3da
Hugo Tapia <hugotfdev0395@gmail.com>
2025-12-13 03:23:52 -0400

    Enable useGlobalPipes with ValidationPipe using whitelist and forbidNonWhitelisted

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //permite pasar todos los datos pero solo los que esten en el dto sin lanzar error
+      forbidNonWhitelisted: true, //lanza un error si se pasan datos que no estan en el dto
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 main().catch((err) => {
